94ff3911a3470f6081ce50a6afc2187dff4abee3
sergeev_denis <sergeev@studiofact.ru>
2021-02-11 17:59:09 +0500

    Initial commit

---
+++ app/src/test/java/digital/fact/saver/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package digital.fact.saver
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

9935a6f05731a2e400de8a196f21f8b01bff5e7b
yunusov <yunusov@studiofact.ru>
2021-02-17 19:01:40 +0500

    #93702# - работал над экраном "мои копилки" и его элементами

--- app/src/main/java/digital/fact/saver/presentation/fragments/bank/BanksFragment.kt
+++ app/src/main/java/digital/fact/saver/presentation/fragments/bank/BanksFragment.kt
@@ -1,12 +1,23 @@
 package digital.fact.saver.presentation.fragments.bank
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import digital.fact.saver.databinding.FragmentBanksBinding
+import digital.fact.saver.databinding.FragmentWalletsBinding
 
 class BanksFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentBanksBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBanksBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }

a8bc5b72de5eced1da514761ceecaaae8d7f5f65
yunusov <yunusov@studiofact.ru>
2021-03-11 18:52:48 +0500

    #93552# - подключал БД к экрану кошельков

---
+++ app/src/main/java/digital/fact/saver/presentation/viewmodels/WalletsViewModel.kt
@@ -0,0 +1,42 @@
+package digital.fact.saver.presentation.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import digital.fact.saver.data.repositories.*
+import digital.fact.saver.domain.models.*
+import digital.fact.saver.domain.repository.*
+
+class WalletsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var sourcesRepository: SourcesRepository
+    var sources: LiveData<List<Source>> = MutableLiveData()
+
+    init {
+        sourcesRepository = SourcesRepositoryImpl(application)
+        sources = sourcesRepository.getAll()
+    }
+
+    fun insertSource(item: Source) {
+        sourcesRepository.insert(item)
+    }
+
+    fun getAllSources(): LiveData<List<Source>> {
+        return sources
+    }
+
+    fun deleteSource(item: Source) {
+        sourcesRepository.delete(item)
+    }
+
+    fun updateSource(item: Source) {
+        sourcesRepository.update(item)
+    }
+
+    fun updateSources() {
+        sourcesRepository.updateAll()
+    }
+
+}

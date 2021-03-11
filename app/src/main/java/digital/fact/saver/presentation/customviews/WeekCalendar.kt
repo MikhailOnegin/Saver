d7140079ccc1225342f04af57dcc6d98eee19ca5
sergeev_denis <sergeev@studiofact.ru>
2021-03-11 10:16:02 +0500

    Заготовка для WeekCalendar

---
+++ app/src/main/java/digital/fact/saver/presentation/customviews/WeekCalendar.kt
@@ -0,0 +1,18 @@
+package digital.fact.saver.presentation.customviews
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class WeekCalendar(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttr: Int
+) : View(context, attrs, defStyleAttr) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+}
--- app/src/main/java/digital/fact/saver/presentation/fragments/history/HistoryFragment.kt
+++ app/src/main/java/digital/fact/saver/presentation/fragments/history/HistoryFragment.kt
@@ -1,12 +1,23 @@
 package digital.fact.saver.presentation.fragments.history
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import digital.fact.saver.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }

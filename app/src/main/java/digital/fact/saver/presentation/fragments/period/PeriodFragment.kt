0573d914f0759a7a3973b2afcc7c4232ba2cfea6
yunusov <yunusov@studiofact.ru>
2021-03-23 18:47:10 +0500

    Настроил вывод данных и калькуляции данных экрана
    #95714#

--- app/src/main/java/digital/fact/saver/presentation/fragments/period/PeriodFragment.kt
+++ app/src/main/java/digital/fact/saver/presentation/fragments/period/PeriodFragment.kt
@@ -5,20 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
-import androidx.preference.PreferenceManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.datepicker.MaterialDatePicker
 import digital.fact.saver.R
 import digital.fact.saver.databinding.FragmentPeriodBinding
-import digital.fact.saver.domain.models.Operation
-import digital.fact.saver.domain.models.Plan
 import digital.fact.saver.domain.models.Source
-import digital.fact.saver.models.*
-import digital.fact.saver.presentation.activity.MainViewModel
+import digital.fact.saver.models.toSources
 import digital.fact.saver.presentation.viewmodels.OperationsViewModel
 import digital.fact.saver.presentation.viewmodels.PeriodViewModel
-import digital.fact.saver.presentation.viewmodels.PlansViewModel
 import digital.fact.saver.presentation.viewmodels.SourcesViewModel
 import digital.fact.saver.utils.*
 import java.text.SimpleDateFormat
@@ -28,6 +23,10 @@ class PeriodFragment : Fragment() {
 
     private lateinit var binding: FragmentPeriodBinding
     private lateinit var periodVM: PeriodViewModel
+    private lateinit var sourceVM: SourcesViewModel
+    private lateinit var operationsVM: OperationsViewModel
+    private lateinit var detailDialog: BottomSheetDialog
+    private var activeSummary: Long = 0L
     private val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
     private val builder = MaterialDatePicker.Builder.dateRangePicker()
     override fun onCreateView(
@@ -36,17 +35,61 @@ class PeriodFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPeriodBinding.inflate(inflater, container, false)
+        detailDialog = BottomSheetDialog(requireActivity())
+        detailDialog.dismissWithAnimation = true
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        periodVM = ViewModelProvider(requireActivity())[PeriodViewModel::class.java]
+        periodVM = ViewModelProvider(requireActivity())[PeriodViewModel::class.java]
+        sourceVM = ViewModelProvider(requireActivity())[SourcesViewModel::class.java]
+        operationsVM = ViewModelProvider(requireActivity())[OperationsViewModel::class.java]
+        setListeners()
         setObservers()
     }
 
     private fun setObservers() {
         periodVM.incomes.observe(viewLifecycleOwner, { onIncomesChanged(it) })
         periodVM.expenses.observe(viewLifecycleOwner, { onExpensesChanged(it) })
+        periodVM.period.observe(viewLifecycleOwner, { onPeriodChanged(it) })
+        sourceVM.sources.observe(viewLifecycleOwner, { onSourcesChanged(it) })
+    }
+
+    private fun onSourcesChanged(sources: List<Source>?) {
+        sources?.let {
+            activeSummary = periodVM.getOperationsResultByDate(
+                sources = it.toSources(
+                    operations = null,
+                    isShowed = false
+                ), period = periodVM.period.value ?: Pair(Date().time, Date().time)
+            )
+            binding.summary.text = activeSummary.toStringFormatter()
+        }
+        val daysCount = periodVM.calculateDaysCount(
+            period = periodVM.period.value ?: Pair(
+                Date().time,
+                Date().time
+            )
+        )
+        binding.hint.text = when (getWordEndingType(daysCount)) {
+            WordEnding.TYPE_1 -> getString(R.string.periodDayHint) + ' ' + daysCount + ' ' + getString(
+                R.string.day_type1
+            )
+            WordEnding.TYPE_2 -> getString(R.string.periodDayHint) + ' ' + daysCount + ' ' + getString(
+                R.string.day_type2
+            )
+            WordEnding.TYPE_3 -> getString(R.string.periodDayHint) + ' ' + daysCount + ' ' + getString(
+                R.string.day_type3
+            )
+        }
+        binding.periodDayBalance.text = (activeSummary.div(daysCount).toStringFormatter())
+    }
+
+    private fun onPeriodChanged(period: Pair<Long, Long>?) {
+        period?.let { setDateToButton(it) }
+        periodVM.getPeriodPlansValues(period ?: Pair(Date().time, Date().time))
     }
 
     private fun onIncomesChanged(it: Long?) {
@@ -57,110 +100,43 @@ class PeriodFragment : Fragment() {
         binding.balanceExpenses.text = it?.toStringFormatter()
     }
 
-//    private fun onSourcesChanged(sources: List<SourceItem>?) {
-//        var saversCount = 0L
-//        var walletsCount = 0L
-//
-//        sources?.forEach { item ->
-//            when (item.itemType) {
-//                Source.SourceCategory.WALLET_ACTIVE.value -> {
-//                    operationsVM.getByDate(
-//                        itemId = item.itemId,
-//                        date = readFromPrefs()
-//                    ).value?.toOperations()?.forEach { operation ->
-//                        when (operation.type) {
-//                            Operation.OperationType.EXPENSES.value, Operation.OperationType.PLANNED_EXPENSES.value -> walletsCount -= operation.sum
-//                            Operation.OperationType.INCOME.value, Operation.OperationType.PLANNED_INCOME.value -> walletsCount += operation.sum
-//                            Operation.OperationType.TRANSFER.value ->
-//                                if (operation.fromSourceId == (item as Sources).id) {
-//                                    walletsCount -= operation.sum
-//                                } else if (operation.toSourceId == item.id) {
-//                                    walletsCount += operation.sum
-//                                }
-//                        }
-//                    }
-//                }
-//                Source.SourceCategory.SAVER.value -> {
-//                    saversCount += (item as Sources).currentSum
-//                }
-//            }
-//        }
-//        if (walletsCount == 0L) walletsCount =
-//            (sources?.firstOrNull { it is SourcesActiveCount } as SourcesActiveCount).activeWalletsSum
-//        val activeSummary = walletsCount - saversCount
-//        binding.summary.text = activeSummary.toStringFormatter()
-//        val daysCount = calculateDaysCount()
-//        binding.hint.text = when (getWordEndingType(daysCount)) {
-//            WordEnding.TYPE_1 -> getString(R.string.periodDayHint) + ' ' + daysCount + ' ' + getString(
-//                R.string.day_type1
-//            )
-//            WordEnding.TYPE_2 -> getString(R.string.periodDayHint) + ' ' + daysCount + ' ' + getString(
-//                R.string.day_type2
-//            )
-//            WordEnding.TYPE_3 -> getString(R.string.periodDayHint) + ' ' + daysCount + ' ' + getString(
-//                R.string.day_type3
-//            )
-//        }
-//        binding.periodDayBalance.text = (activeSummary.div(daysCount).toStringFormatter())
-//    }
-//
-//    private fun calculateDaysCount(): Int {
-//        val inMillis = readFromPrefs(false) - readFromPrefs()
-//        return inMillis.div(3600000L * 24).toInt()
-//    }
-//
-//    private fun setData() {
-//        setDateToButton(readFromPrefs(), readFromPrefs(false))
-//    }
-//
-//    private fun onPlansChanged(list: List<Plan>) {
-//        val incomes = list.filter { it.type == Plan.PlanType.INCOME.value }
-//        val expenses = list.filter { it.type == Plan.PlanType.SPENDING.value }
-//        var incomesSum = 0L
-//        var expensesSum = 0L
-//
-//        incomes.forEach { incomesSum += it.sum }
-//        expenses.forEach { expensesSum += it.sum }
-//
-//        binding.balanceIncome.text = incomesSum.toStringFormatter()
-//        binding.balanceExpenses.text = expensesSum.toStringFormatter()
-//    }
-//
-//    private fun setListeners() {
-//        binding.period.setOnClickListener { showDatePicker() }
-//    }
-//
-//    private fun showDatePicker() {
-//        builder.setTheme(R.style.Calendar)
-//        val picker = builder.build()
-//        picker.addOnPositiveButtonClickListener {
-//            if (it.first == it.second) {
-//                createSnackBar(
-//                    binding.root,
-//                    getString(R.string.errorPeriodDays)
-//                ).show()
-//            } else {
-//                writeToPrefs(it.first, it.second)
-//                setDateToButton(it.first, it.second)
-//                sourcesVM.updateSources()
-//            }
-//        }
-//        picker.show(childFragmentManager, "date_picker")
-//    }
-//
-//    private fun setDateToButton(first: Long?, second: Long?) {
-//        val stringBuilder = StringBuilder()
-//        stringBuilder.append(getString(R.string.period))
-//        stringBuilder.append(": ")
-//        stringBuilder.append(sdf.format(first))
-//        stringBuilder.append(" - ")
-//        stringBuilder.append(sdf.format(second))
-//        binding.period.text = stringBuilder
-//    }
+    private fun setListeners() {
+        binding.period.setOnClickListener { showDatePicker() }
+        binding.instructions.setOnClickListener { showCalculateInstructions() }
+    }
 
-    companion object {
-        const val PREF_PLANNED_PERIOD_FROM = "pref_planned_period_from"
-        const val PREF_PLANNED_PERIOD_TO = "pref_planned_period_to"
+    private fun showCalculateInstructions() {
+
+    }
+
+    private fun showDatePicker() {
+        builder.setTheme(R.style.Calendar)
+        val picker = builder.build()
+        picker.addOnPositiveButtonClickListener {
+            if (it.first == it.second) {
+                createSnackBar(
+                    binding.root,
+                    getString(R.string.errorPeriodDays)
+                ).show()
+            } else {
+                periodVM.writeToPrefs(it.first, it.second)
+                val period =
+                    Pair(first = it.first ?: Date().time, second = it.second ?: Date().time)
+                setDateToButton(period)
+                sourceVM.updateSources()
+            }
+        }
+        picker.show(childFragmentManager, "date_picker")
+    }
+
+    private fun setDateToButton(period: Pair<Long, Long>) {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append(getString(R.string.period))
+        stringBuilder.append(": ")
+        stringBuilder.append(sdf.format(period.first))
+        stringBuilder.append(" - ")
+        stringBuilder.append(sdf.format(period.second))
+        binding.period.text = stringBuilder
     }
 
 }
--- app/src/main/java/digital/fact/saver/presentation/viewmodels/PeriodViewModel.kt
+++ app/src/main/java/digital/fact/saver/presentation/viewmodels/PeriodViewModel.kt
@@ -6,8 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.preference.PreferenceManager
 import digital.fact.saver.App
+import digital.fact.saver.domain.models.Operation
 import digital.fact.saver.domain.models.Plan
-import digital.fact.saver.presentation.fragments.period.PeriodFragment
+import digital.fact.saver.domain.models.Source
+import digital.fact.saver.models.SourceItem
+import digital.fact.saver.models.Sources
+import digital.fact.saver.models.SourcesActiveCount
+import digital.fact.saver.models.toOperations
 import digital.fact.saver.utils.resetDate
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,9 +21,10 @@ import java.util.*
 class PeriodViewModel : ViewModel() {
 
     private var app: App = App.getInstance()
+    private val prefs = PreferenceManager.getDefaultSharedPreferences(app.applicationContext)
 
     init {
-        getPeriodPlansValues()
+        readPrefsFromDisk()
     }
 
     private val _incomes = MutableLiveData<Long>()
@@ -26,10 +32,10 @@ class PeriodViewModel : ViewModel() {
     private val _expenses = MutableLiveData<Long>()
     val expenses: LiveData<Long> = _expenses
 
-    private fun getPeriodPlansValues() {
+    fun getPeriodPlansValues(period: Pair<Long, Long>) {
         viewModelScope.launch(Dispatchers.IO) {
             val result = App.db.plansDao()
-                .getPlansByPeriod(periodStart = readFromPrefs(), periodEnd = readFromPrefs(false))
+                .getPlansByPeriod(periodStart = period.first, periodEnd = period.second)
             var incomes = 0L
             var expenses = 0L
             result.forEach {
@@ -43,25 +49,74 @@ class PeriodViewModel : ViewModel() {
         }
     }
 
-    private fun readFromPrefs(first: Boolean = true): Long {
-        val calendar = Calendar.getInstance(Locale.getDefault())
-        calendar.time = Date()
-        calendar.add(Calendar.DAY_OF_MONTH, 30)
-        val nextMonth = calendar.timeInMillis
-        val prefs = PreferenceManager.getDefaultSharedPreferences(app.applicationContext)
-        return if (first)
-            prefs.getLong(PeriodFragment.PREF_PLANNED_PERIOD_FROM, Date().time)
-        else {
-            prefs.getLong(PeriodFragment.PREF_PLANNED_PERIOD_TO, nextMonth)
+    fun getOperationsResultByDate(sources: List<SourceItem>, period: Pair<Long, Long>): Long {
+        var saversCount = 0L
+        var walletsCount = 0L
+        sources.forEach { item ->
+            when (item.itemType) {
+                Source.SourceCategory.WALLET_ACTIVE.value -> {
+                    App.db.operationsDao().getByDate(
+                        itemId = item.itemId,
+                        date = period.second
+                    ).toOperations().forEach { operation ->
+                        when (operation.type) {
+                            Operation.OperationType.EXPENSES.value, Operation.OperationType.PLANNED_EXPENSES.value -> walletsCount -= operation.sum
+                            Operation.OperationType.INCOME.value, Operation.OperationType.PLANNED_INCOME.value -> walletsCount += operation.sum
+                            Operation.OperationType.TRANSFER.value ->
+                                if (operation.fromSourceId == (item as Sources).id) {
+                                    walletsCount -= operation.sum
+                                } else if (operation.toSourceId == item.id) {
+                                    walletsCount += operation.sum
+                                }
+                        }
+                    }
+                }
+                Source.SourceCategory.SAVER.value -> {
+                    saversCount += (item as Sources).currentSum
+                }
+            }
+        }
+        if (walletsCount == 0L) walletsCount =
+            (sources.firstOrNull { it is SourcesActiveCount } as SourcesActiveCount).activeWalletsSum
+        return walletsCount - saversCount
+    }
+
+    fun calculateDaysCount(period: Pair<Long, Long>): Int {
+        val inMillis = period.second - period.first
+        return inMillis.div(3600000L * 24).toInt()
+    }
+
+    private val _period = MutableLiveData<Pair<Long, Long>>()
+    val period: LiveData<Pair<Long, Long>> = _period
+
+    private fun readPrefsFromDisk() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val calendar = Calendar.getInstance(Locale.getDefault())
+            calendar.time = Date()
+            calendar.add(Calendar.DAY_OF_MONTH, 30)
+            val nextMonth = calendar.timeInMillis
+            _period.postValue(
+                Pair(
+                    first = prefs.getLong(PREF_PLANNED_PERIOD_FROM, resetDate(Date().time)),
+                    second = prefs.getLong(PREF_PLANNED_PERIOD_TO, resetDate(nextMonth))
+                )
+            )
         }
     }
 
     fun writeToPrefs(first: Long?, second: Long?) {
-        val prefs = PreferenceManager.getDefaultSharedPreferences(app.applicationContext)
-        prefs.edit()
-            .putLong(PeriodFragment.PREF_PLANNED_PERIOD_FROM, resetDate(first ?: 0L))
-            .putLong(PeriodFragment.PREF_PLANNED_PERIOD_TO, resetDate(second ?: 0L))
-            .apply()
+        viewModelScope.launch(Dispatchers.IO) {
+            prefs.edit()
+                .putLong(PREF_PLANNED_PERIOD_FROM, resetDate(first ?: 0L))
+                .putLong(PREF_PLANNED_PERIOD_TO, resetDate(second ?: 0L))
+                .apply()
+            readPrefsFromDisk()
+        }
+    }
+
+    companion object {
+        const val PREF_PLANNED_PERIOD_FROM = "pref_planned_period_from"
+        const val PREF_PLANNED_PERIOD_TO = "pref_planned_period_to"
     }
 
 }
